a18ce64cc7b6fff82aeb139d763cdfc3ad5fbf71
Adrian Havenga-Bennett <adrian.havenga@yahoo.com>
2019-12-07 17:00:36 +0000

    added category delete confirm / added further readings database and ability to add terms to it

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask_pymongo import pymongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "3ef;EFJH;DUGHA;dsjgn;IUDGS;ADJBV;DLSIUHGA;LSDJGHP;DUXH;UHNP;piouhpIUEHA;GU;RAWERAG;ODFIGH;AHG;AIOSUDG"
 
 MONGODB_URI = os.getenv("MONGO_URI")
 DBS_NAME = "terming_for_learning"
 TERMS = "terms"
 CATEGORIES = "categories"
+FURTHER_READINGS = "further_readings"
 
 
 def mongo_connect(url):
@@ -22,6 +24,7 @@ def mongo_connect(url):
 conn = mongo_connect(MONGODB_URI)
 terms = conn[DBS_NAME][TERMS]
 categories = conn[DBS_NAME][CATEGORIES]
+further_readings = conn[DBS_NAME][FURTHER_READINGS]
 
 # Home screen / main welcome page:
 @app.route("/")
@@ -37,7 +40,8 @@ def get_terms():
 @app.route("/term/<term_id>")
 def show_term(term_id):
     term = terms.find_one({"_id": ObjectId(term_id)})
-    return render_template("term.html", term=term)
+    return render_template("term.html", term=term,
+                           further_readings=further_readings.find())
 
 # Add a new term:
 @app.route("/new_term")
@@ -79,14 +83,10 @@ def delete_request(term_id):
     term = terms.find_one({"_id": ObjectId(term_id)})
     return render_template("delete_request.html", term=term)
 
-# Shows chosen term and asks for confirmation:
+# Shows chosen term and asks for confirmation before deleting:
 @app.route("/delete_term/<term_id>")
 def delete_term(term_id):
-    """
-    At the moment, this will delete the term immediately.
-    You'll need to edit it later so that it asks first.
-    """
-    terms.remove({"_id": ObjectId(term_id)})
+    terms.delete_one({"_id": ObjectId(term_id)})
     return redirect(url_for("get_terms"))
 
 # Show all categories:
@@ -100,14 +100,15 @@ def show_category(category_id):
     category = categories.find_one({"_id": ObjectId(category_id)})
     return render_template("show_category.html", category=category)
 
-
 # Delete a category:
+@app.route("/delete_category_request/<category_id>")
+def delete_category_request(category_id):
+    category = categories.find_one({"_id": ObjectId(category_id)})
+    return render_template("delete_category_request.html", category=category)
+
+# Shows chosen category and asks for confirmation before deleting:
 @app.route("/delete_category/<category_id>")
 def delete_category(category_id):
-    """
-    Note that this will delete the
-    category without confirmation
-    """
     categories.remove({"_id": ObjectId(category_id)})
     return redirect(url_for("get_categories"))
 
@@ -126,6 +127,29 @@ def save_category():
     else:
         categories.insert_one(request.form.to_dict())
     return redirect(url_for("get_categories"))
+
+
+# Shows the list of saved terms (Further Readings):
+@app.route("/saved_terms")
+def saved_terms():
+    return render_template("further_readings.html",
+                           further_readings=further_readings.find())
+
+
+# Copies a document and saves it to the further readings database:
+@app.route("/add_to_saved/<term_id>")
+def add_to_saved(term_id):
+    term = terms.find_one({"_id": ObjectId(term_id)})
+    further_readings.insert_one(term)
+    return redirect(url_for("saved_terms"))
+
+
+# Removes a document from the further readings database:
+@app.route("/remove_from_saved/<term_id>")
+def remove_from_saved(term_id):
+    term = terms.find_one({"_id": ObjectId(term_id)})
+    further_readings.remove(term)
+    return redirect(url_for("saved_terms"))
 
 
 if __name__ == "__main__":
